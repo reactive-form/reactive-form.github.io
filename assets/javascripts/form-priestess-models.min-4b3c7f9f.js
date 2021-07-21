cb0a600b92026132751942b91e9d657a1123a422
wangluyuan <wangluyuan@jilinxiangyun.com>
2021-07-21 08:51:03 +0800

    chore: yarn run build script

--- assets/javascripts/form-priestess-models.min-4b3c7f9f.js
+++ assets/javascripts/form-priestess-models.min-4b3c7f9f.js
@@ -3,4 +3,4 @@
  * Copyright 2021 JLxiangyun | MIT license
  */
 (()=>{var t={},e=t;})();
-//# sourceMappingURL=form-priestess-models.min.js-fd617c2e.map
+//# sourceMappingURL=form-priestess-models.min.js-8ea1fadc.map
--- assets/javascripts/form-priestess-react.min-c77bf826.js
+++
@@ -1,6 +0,0 @@
-/*!
- * form-priestess 0.1.5 | React | https://jilinxiangyun.com
- * Copyright 2021 JLxiangyun | MIT license
- */
-(()=>{var t={},e=t;})();
-//# sourceMappingURL=form-priestess-react.min.js-63d8b739.map
