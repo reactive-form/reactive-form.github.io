551f35e4cf0fc5c336be6a1074c3e3af5b9951fe
oatw <oatwoatw@icloud.com>
2021-07-26 13:35:42 +0800

    Refactor js bundle pipeline.

--- assets/javascripts/form-priestess-helpers.min-352a3dcf.js
+++
@@ -1,6 +0,0 @@
-/*!
- * form-priestess 0.1.5 | Helpers | https://jilinxiangyun.com
- * Copyright 2021 JLxiangyun | MIT license
- */
-(()=>{var t={},e=t;})();
-//# sourceMappingURL=form-priestess-helpers.min.js-abeea740.map
--- assets/javascripts/form-priestess-models.min-4b3c7f9f.js
+++
@@ -1,6 +0,0 @@
-/*!
- * form-priestess 0.1.5 | Models | https://jilinxiangyun.com
- * Copyright 2021 JLxiangyun | MIT license
- */
-(()=>{var t={},e=t;})();
-//# sourceMappingURL=form-priestess-models.min.js-8ea1fadc.map
