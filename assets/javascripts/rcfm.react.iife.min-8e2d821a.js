9c9eb550ed4201269eb9604344908eadec6356f0
oatw <oatwoatw@icloud.com>
2021-12-09 08:58:39 +0800

    Build v0.2.4

--- assets/javascripts/rcfm.react.iife.min-8e2d821a.js
+++
@@ -1,6 +0,0 @@
-/*!
- * rcfm 0.2.3 | React iife | https://jilinxiangyun.com
- * Copyright 2021 JLxiangyun | MIT license
- */
-(()=>{(()=>{let r=window.require,i=e=>({"rcfm/helper":{UI:window.ReactiveForm.UI},react:window.React,"react-dom":window.ReactDOM})[e];window.require=e=>r&&r(e)||i(e)})();})();
-//# sourceMappingURL=rcfm.react.iife.min.js-192c248c.map
